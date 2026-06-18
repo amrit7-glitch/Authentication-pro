8622ec5e801c20a44dd90dcd3171d4bf9dad0f84
Amrit <amritsmc02@gmail.com>
2026-06-18 14:47:13 +0530

    Refactor email transporter configuration to use secure SMTP settings and improve connection timeouts

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -3,18 +3,15 @@ import nodemailer from "nodemailer";
 
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    auth: {
-        user: process.env.EMAIL,
-        pass: process.env.APP_PASSWORD
-    },
-    connectionTimeout: 10000,
-    greetingTimeout: 10000,
-    socketTimeout: 10000,
-    debug: true,
-    logger: true
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.APP_PASSWORD,
+  },
+  logger: true,
+  debug: true,
 });
 
 export const sendEmail = async (
